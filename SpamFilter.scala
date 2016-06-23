bd892a9baee66bca2520d3bb0801dfb257179105
camilleshainne <camilleshainne@gmail.com>
2016-06-23 14:56:36 +0800

    Created Spam Filter in Scala

--- SpamFilter.scala
+++ SpamFilter.scala
@@ -1,62 +1,160 @@
-import java.io.File
-import java.io.FileNotFoundException
-import java.io.IOException
-import scala.collection.immutable.Map
-import scala.collection.mutable.ArrayBuffer
+import java.io._
+import java.lang._
+import java.util._
+import scala.collection.JavaConversions._
 import scala.io.Source
+import scala.util.control._
 
 object SpamFilter{
 	def main(args: Array[String]){
-		var spam = new BagOfWords(args(0))	// second argument for spam training data
-		var ham = new BagOfWords(args(1))	// third argument for ham training data
-		var message : Map[String, Double] = Map()
+		var spam = args(1)
+		var ham = args(2)
+		var spamfilter = new SpamFilter(spam, ham)
+
+		spamfilter.filterSpam(new File(args(0)))
+		spamfilter.filter()
+		spamfilter.saveOutput()
+	}
+
+	class SpamFilter(spamFile: String, hamFile: String) {
+		var spam = new BagOfWords(spamFile)	// second argument for spam training data
+		var ham = new BagOfWords(hamFile)	// third argument for ham training data
+		var message = new Hashtable[String, Double]();
+		var category = new Hashtable[String, String]()
 		var totalSize : Int = 0
 		var totalLines : Int = 0
 		var k : Int = 0
 
-		totalSize = getDictionarySize(new File(args(0)), new File(args(1)))
+		totalSize = this.getDictionarySize()
 		totalLines = spam.lines + ham.lines
 
-		println("Total Dictionary Size: " + totalSize)
-		println("Total Number of Lines: " + totalLines)
-	}
+		def getDictionarySize() : Int = {
+			var dictionarySize : Int = spam.dictionarySize
+			var key = new String
 
-	def getDictionarySize(spam: File, ham: File) : Int = {
-		var size : Int = 0
-		var table : Map[String, Int] = Map()
-		var table1 : Map[String, Int] = Map()
-		var table2 : Map[String, Int] = Map()
-		var s = Source.fromFile(spam).mkString
-		var h = Source.fromFile(ham).mkString
+			for(key <- ham.bagOfWords.keySet()){
+				if(!spam.bagOfWords.containsKey(key)) dictionarySize += 1
+			}
+			
+			return dictionarySize
+		}
 
-		s = s.toLowerCase().replaceAll("[^a-zA-Z0-9\\s]", "")
-		table1 = s.split("\\s+").groupBy(x=>x).mapValues(x=>x.length)
-		h = h.toLowerCase().replaceAll("[^a-zA-Z0-9\\s]", "")
-		table2 = h.split("\\s+").groupBy(x=>x).mapValues(x=>x.length)
+		def filterSpam(file: File) {
+			var input : ArrayList[String] = this.loadFile(file)
+			var m = new String
 
-		table = table1 ++ table2
-		size = table.size
+			var pWs : Double = 1.0		// P(message|spam)
+			var pWh : Double = 1.0		// P(message|ham)
+			var pSpam : Double = ((spam.lines + this.k).toDouble) / ((totalLines + (2 * k)).toDouble)	// P(spam)
+			var pHam : Double = ((ham.lines + this.k).toDouble) / ((totalLines + (2 * k)).toDouble)	// P(ham)
+			var pSm : Double = 0.0		// P(spam|message)
+			var msg = new String
 
-		return size
-	}
+			for(msg <- input){
+				var num : Int = 0
+				var string : Array[String] = msg.split(" ")
 
-	def filterSpam(file: File) {
+				for(m <- string){
+					if(!spam.bagOfWords.containsKey(m) && !ham.bagOfWords.containsKey(m)) num += 1
+				}
 
-	}
+				for(m <- string){
+					var s : Int = 0
+					var h : Int = 0
 
-	def loadFile(file: File) : ArrayBuffer{
+					var sp = new java.lang.Integer(spam.bagOfWords.get(m))
+					var ha = new java.lang.Integer(ham.bagOfWords.get(m))
 
+					if(sp == null) s = 0
+					else s = sp
+					pWs *= (s + this.k).toDouble / (spam.totalWords + (this.k * (totalSize + num))).toDouble
+
+					if(ha == null) h = 0
+					else h = ha
+					pWh *= (h + this.k).toDouble / (ham.totalWords + (this.k * (totalSize + num))).toDouble
+				}
+
+				pSm = (pWs * pSpam) / ((pWs * pSpam) + (pWh * pHam)) // P(spam|message)
+				message.put(msg, pSm)
+			}
+		}
+
+		def loadFile(file: File) : ArrayList[String] = {
+			var msg = new ArrayList[String]()
+			val loop = new Breaks
+
+			try{
+				var read = new FileReader(file)
+				var buffer = new BufferedReader(read)
+				var word = buffer.readLine()
+				this.k = java.lang.Integer.parseInt(word)
+
+				loop.breakable{
+					while(word != null){
+						word = buffer.readLine()
+						if(word == null) loop.break
+						word = word.replaceAll("[^a-zA-Z0-9 ]", "")
+						msg.add(word.toLowerCase())
+					}
+				}
+
+				buffer.close()
+			} catch {
+				case ex: FileNotFoundException => {
+					println("Missing file exception")
+					return null
+				}
+				case ex: IOException => {
+					println("IOException")
+					return null
+				}
+			}
+
+			return msg
+		}
+
+		def filter() {
+			var key = new String
+
+			for(key <- this.message.keySet()){
+				if(this.message.get(key) > 0.5) this.category.put(key, "SPAM")
+				else this.category.put(key, "HAM")
+			}
+		}
+
+		def saveOutput() : Boolean = {
+			try{
+				var file = new File("output.txt")
+
+				if(!file.exists()) file.createNewFile()
+
+				var writer = new FileWriter(file)
+				var buffer = new BufferedWriter(writer)
+				var key = new String
+
+				for(key <- this.category.keySet()) buffer.write(key + " - " + this.category.get(key) + "\n")
+				
+				buffer.close()
+				true
+			} catch {
+				case ex: FileNotFoundException => {
+					println("Missing file exception")
+					false
+				}
+				case ex: IOException => {
+					println("IOException")
+					false
+				}
+			}
+		}
 	}
 
 	class BagOfWords(input: String){
-		var bagOfWords : Map[String,Int] = Map()
-		var words = new ArrayBuffer()
-		var bag = new ArrayBuffer()
+		var bagOfWords = new Hashtable[String, Int]()
+		var words = new ArrayList[String]()
 		var lines : Int = _
 		var dictionarySize : Int = _
 		var totalWords : Int = _
-
-		bagOfWords = listWords(new File(input))
 
 		try {
 			loadWords(new File(input))
@@ -71,18 +169,40 @@ object SpamFilter{
 			}
 		}
 
-		dictionarySize = bagOfWords.size
-		for(sum <- bagOfWords.values) totalWords += sum
-		println("Dictionary Size: " + dictionarySize)
-		println("Total Words: " + totalWords)
-		println(listWords(new File(input)))
+		bagOfWords = listWords(words)
+		dictionarySize = bagOfWords.keySet().size()
+		totalWords = words.size()
 
 		def loadWords(file: File): Boolean = {
+			try{
+				var scanner = new Scanner(file)
+
+				while(scanner.hasNext()){
+					var addWord = scanner.next
+					addWord = addWord.replaceAll("[^a-zA-Z0-9 ]", "")
+					words.add(addWord.toLowerCase())
+				}
+
+				scanner.close()
+			} catch {
+				case ex: FileNotFoundException => {
+					println("Missing file exception")
+					false
+				}
+				case ex: IOException => {
+					println("IOException")
+					false
+				}
+			}
 
 			try {
-				for(line <- Source.fromFile(file).getLines) lines += 1
-			}
-			catch {
+				var read = new FileReader(file)
+				var buffer = new BufferedReader(read)
+
+				while(buffer.readLine() != null) lines += 1
+				
+				buffer.close()
+			} catch {
 				case ex: FileNotFoundException => {
 					println("Missing file exception")
 					false
@@ -96,12 +216,14 @@ object SpamFilter{
 			true
 		}
 
-		def listWords(file: File) : Map[String, Int] = {
-			var table : Map[String, Int] = Map()
+		def listWords(w: ArrayList[String]) : Hashtable[String, Int] = {
+			var table = new Hashtable[String, Int]()
+			var s = new String
 
-			var addWord = Source.fromFile(file).mkString // returns the file as String
-			addWord = addWord.toLowerCase().replaceAll("[^a-zA-Z0-9\\s]", "")
-			table = addWord.split("\\s+").groupBy(x=>x).mapValues(x=>x.length)
+			for(s <- w){
+				if(table.containsKey(s)) table.put(s, table.get(s)+1)
+				else table.put(s, 1) 	// new word
+			}
 
 			return table
 		}
